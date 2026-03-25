07f330f0f0c00b9baea2f9c250032df58d9d06d5
Nikhil2261 <132556436+Nikhil2261@users.noreply.github.com>
2026-03-25 15:12:57 +0530

    console for ping

--- src/routes/ping.routes.js
+++ src/routes/ping.routes.js
@@ -2,7 +2,19 @@ import express from "express";
 
 const router = express.Router();
 
+// router.get("/ping", (req, res) => {
+//   res.status(200).send("OK");
+// });
 router.get("/ping", (req, res) => {
+
+  const time = new Date().toISOString();
+
+  const ip =
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress;
+
+  console.log(`📡 [PING] ${time} | IP: ${ip}`);
+
   res.status(200).send("OK");
 });
 
