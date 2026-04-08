37a28765cfc40c222229e30e07e02394521cab74
Nikhil2261 <132556436+Nikhil2261@users.noreply.github.com>
2026-04-08 12:06:45 +0530

    fan controllers

--- src/mqtt/telemetryHandler.js
+++ src/mqtt/telemetryHandler.js
@@ -1,94 +1,4 @@
 
-
-// import Device from "../models/Device.js";
-// import { emitToUser } from "../socket/index.js";
-
-// export async function handleTelemetry(topic, payload) {
-
-//   let data;
-
-//   try {
-//     data = JSON.parse(payload.toString());
-//   } catch (err) {
-//     console.error("Invalid telemetry JSON");
-//     return;
-//   }
-
-//   const parts = topic.split("/");
-//   const deviceId = parts[1];
-
-//   if (!deviceId) return;
-
-//   /* OTA PROGRESS */
-
-//   if (data.type === "ota") {
-
-//     const device = await Device.findOne({ deviceId });
-
-//     if (!device?.owner) return;
-
-//     emitToUser(device.owner.toString(), "ota", {
-//       deviceId,
-//       stage: data.stage,
-//       percent: data.percent ?? 0,
-//       ts: data.ts ?? Date.now()
-//     });
-
-//     return;
-//   }
-
-//   /* TELEMETRY NORMALIZATION */
-
-//   if (!Array.isArray(data.states)) {
-//     data.states = [];
-//   }
-
-//   const normalizedStates = [];
-//   let fan = null;
-
-//   for (const s of data.states) {
-
-//     if (s.type === "switch" && typeof s.pin === "number") {
-//       normalizedStates.push({
-//         type: "switch",
-//         pin: s.pin,
-//         status: s.status
-//       });
-//     }
-
-//     if (s.type === "fan") {
-//       fan = s.speed;
-//     }
-
-//   }
-
-//   const device = await Device.findOneAndUpdate(
-//     { deviceId },
-//     {
-//       $set: {
-//         states: normalizedStates,
-//         fanSpeed: fan,
-//         firmware: data.fw,
-//         online: true,
-//         lastSeen: new Date()
-//       }
-//     },
-//     { new: true }
-//   );
-
-//   if (!device?.owner) return;
-
-//   emitToUser(device.owner.toString(), "telemetry", {
-//     deviceId,
-//     states: device.states,
-//     fanSpeed: device.fanSpeed,
-//     firmware: device.firmware,
-//     ts: data.ts ?? Date.now()
-//   });
-
-// }
-
-
 import Device from "../models/Device.js";
 import Firmware from "../models/Firmware.js";
 import { emitToUser } from "../socket/index.js";
@@ -121,35 +31,70 @@ export async function handleTelemetry(topic, payload) {
   }
 
   /* TELEMETRY NORMALIZATION */
-  if (!Array.isArray(data.states)) data.states = [];
+if (!Array.isArray(data.states)) data.states = [];
 
-  const normalizedStates = [];
-  let fan = null;
+const normalizedStates = [];
 
-  for (const s of data.states) {
-    if (s.type === "switch" && typeof s.pin === "number") {
-      normalizedStates.push({
-        type: "switch",
-        pin: s.pin,
-        status: s.status
-      });
-    }
-    if (s.type === "fan") fan = s.speed;
+for (const s of data.states) {
+  if (s.type === "switch" && typeof s.pin === "number") {
+    normalizedStates.push({
+      type: "switch",
+      pin: s.pin,
+      status: s.status
+    });
   }
 
-  const device = await Device.findOneAndUpdate(
-    { deviceId },
-    {
-      $set: {
-        states: normalizedStates,
-        fanSpeed: fan,
-        firmware: data.fw,
-        online: true,
-        lastSeen: new Date()
-      }
-    },
-    { new: true }
-  );
+  if (s.type === "fan") {
+    normalizedStates.push({
+      type: "fan",
+      speed: s.speed ?? 0
+    });
+  }
+}
+
+const device = await Device.findOneAndUpdate(
+  { deviceId },
+  {
+    $set: {
+      states: normalizedStates,
+      firmware: data.fw,
+      online: true,
+      lastSeen: new Date()
+    }
+  },
+  { new: true }
+);
+  
+  // /* TELEMETRY NORMALIZATION */ =================================old dashboard code
+  // if (!Array.isArray(data.states)) data.states = [];
+
+  // const normalizedStates = [];
+  // let fan = null;
+
+  // for (const s of data.states) {
+  //   if (s.type === "switch" && typeof s.pin === "number") {
+  //     normalizedStates.push({
+  //       type: "switch",
+  //       pin: s.pin,
+  //       status: s.status
+  //     });
+  //   }
+  //   if (s.type === "fan") fan = s.speed;
+  // }
+
+  // const device = await Device.findOneAndUpdate(
+  //   { deviceId },
+  //   {
+  //     $set: {
+  //       states: normalizedStates,
+  //       fanSpeed: fan,
+  //       firmware: data.fw,
+  //       online: true,
+  //       lastSeen: new Date()
+  //     }
+  //   },
+  //   { new: true }
+  // );
 
   if (!device?.owner) return;
 
@@ -168,11 +113,19 @@ export async function handleTelemetry(topic, payload) {
     }
   }
 
-  emitToUser(device.owner.toString(), "telemetry", {
-    deviceId,
-    states: device.states,
-    fanSpeed: device.fanSpeed,
-    firmware: device.firmware,
-    ts: data.ts ?? Date.now()
-  });
+//   emitToUser(device.owner.toString(), "telemetry", {
+//     deviceId,
+//     states: device.states,
+//     fanSpeed: device.fanSpeed,
+//     firmware: device.firmware,
+//     ts: data.ts ?? Date.now()
+//   });
+// }
+
+emitToUser(device.owner.toString(), "telemetry", {
+  deviceId,
+  states: device.states,
+  firmware: device.firmware,
+  ts: data.ts ?? Date.now()
+});
 }
