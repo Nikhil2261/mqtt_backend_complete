d01b5e85ca279600e7fb98c0aad9e5d1cfcd7bd9
Nikhil2261 <132556436+Nikhil2261@users.noreply.github.com>
2026-04-10 12:08:42 +0530

    ackhandler_logs_add

--- src/mqtt/ackHandler.js
+++ src/mqtt/ackHandler.js
@@ -1,74 +1,22 @@
 
-// import Command from "../models/Command.js";
-// import Device from "../models/Device.js";
-// import { emitToUser } from "../socket/index.js";
-
-// export async function handleAck(topic, payload) {
-
-//   let data;
-
-//   try {
-//     data = JSON.parse(payload.toString());
-//   } catch {
-//     return;
-//   }
-
-//   const parts = topic.split("/");
-//   const deviceId = parts[1];
-
-//   if (!deviceId || !data.cmdId) return;
-
-//   let status = "done";
-
-//   if (data.status === "failed") {
-//     status = "failed";
-//   }
-
-//   const res = await Command.updateOne(
-//     {
-//       deviceId,
-//       cmdId: data.cmdId,
-//       status: { $ne: "done" }
-//     },
-//     {
-//       $set: {
-//         status,
-//         ackAt: new Date(),
-//         failedReason: status === "failed" ? "device_reject" : null
-//       }
-//     }
-//   );
-
-//   if (res.matchedCount === 0) return;
-
-//   const device = await Device.findOne({ deviceId });
-
-//   if (!device?.owner) return;
-
-//   emitToUser(device.owner.toString(), "ack", {
-//     deviceId,
-//     cmdId: data.cmdId,
-//     status,
-//     ts: Date.now()
-//   });
-
-// }
-
 import Command from "../models/Command.js";
 import Device from "../models/Device.js";
 import { emitToUser } from "../socket/index.js";
 
 export async function handleAck(topic, payload) {
 
+  console.log("📩 ACK RAW:", payload.toString());
   let data;
   try {
     data = JSON.parse(payload.toString());
+    console.log("✅ ACK parsed:", data);
   } catch {
     return;
   }
 
   const parts = topic.split("/");
   const deviceId = parts[1];
+  console.log("📦 Device ID:", deviceId);
 
   if (!deviceId || !data.cmdId) return;
 
@@ -90,6 +38,7 @@ export async function handleAck(topic, payload) {
       }
     }
   );
+  console.log("🧾 Command update result:", res);
 
   if (res.matchedCount === 0) return;
 
@@ -126,4 +75,5 @@ export async function handleAck(topic, payload) {
     status,
     ts: Date.now()
   });
+  console.log("🚀 EMITTING ACK TO USER:", device.owner.toString());
 }
